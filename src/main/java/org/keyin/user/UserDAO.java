b1597dcf2f648b5d94adee2c5419375c4ac968e0
Jordan Kelloway <106637327+JKells99@users.noreply.github.com>
2025-03-13 14:55:45 -0230

    Added password hasing, added DAO method to get all users

--- src/main/java/org/keyin/user/UserDAO.java
+++ src/main/java/org/keyin/user/UserDAO.java
@@ -4,6 +4,7 @@ import org.keyin.database.DatabaseConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDAO {
@@ -27,6 +28,38 @@ public class UserDAO {
         }
 
     }
+
+    public void getAllUsers() throws SQLException {
+        ResultSet rs = null;
+        String sql = "SELECT * FROM users";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            rs = pstmt.executeQuery();
+            System.out.println("Users in the database");
+            while (rs.next()) {
+                int user_id = rs.getInt("user_id");
+                String username = rs.getString("username");
+                String email = rs.getString("email");
+                String password = rs.getString("password");
+                String role = rs.getString("role");
+                String sector = rs.getString("sector");
+
+                System.out.println("-------------------------------");
+                System.out.println();
+                System.out.println("Username: " + username);
+                System.out.println("Email: " + email);
+                System.out.println("Password: " + password);
+                System.out.println("Role: " + role);
+                System.out.println("Sector: " + sector);
+                System.out.println();
+                System.out.println("------------------------------------");
+
+            }
+
+        };
+    }
+
+
 
 
 }
--- src/main/java/org/keyin/user/UserService.java
+++ src/main/java/org/keyin/user/UserService.java
@@ -1,14 +1,31 @@
 package org.keyin.user;
 
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.SQLException;
+
 public class UserService {
 
     private UserDAO userDAO = new UserDAO();
 
 
 
-    public void createNewUser(User user){
+    public boolean createNewUser(User user){
+        if( user.equals(null)){
+            System.out.println("User is null please provide a valid user");
+            return false;
+        }
+        String hashedPassword = BCrypt.hashpw(user.getPassword(),BCrypt.gensalt(8));
+
+        user.setPassword(hashedPassword);
+
         userDAO.createNewUser(user);
         System.out.println("User created successfully");
+        return true;
+    }
+
+    public void printAllUsersInSystem() throws SQLException {
+        userDAO.getAllUsers();
     }
 
 
